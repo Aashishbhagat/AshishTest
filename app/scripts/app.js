2b449b7439063f028147818570edb2bb6b234b4b
Bhagat <ashish.bhagat@metronom-external.com>
2021-05-13 12:32:39 +0530

    13.05.2021:AB:Fetching All tickets and showing all ticket in the page and showing all tickets ids in the select button

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,4 @@
+var domain = "https://metrocihppde.freshdesk.com";  var auth ="TdaXqo2eMPjcMyN7IW3";
 document.onreadystatechange = function () {
   if (document.readyState === 'interactive') renderApp();
 
@@ -5,17 +6,31 @@ document.onreadystatechange = function () {
     var onInit = app.initialized();
 
     onInit.then(getClient).catch(handleErr);
+   // onInit.then(intilizeClient).catch(handleErr);
 
     function getClient(_client) {
       window.client = _client;
+      client.iparams.get().then(
+        function(data) {
+                //console.log(data);					
+          auth = "Basic " + btoa(auth);
+         // domain = data.domain;
+         // alert("domain name:-" +JSON.stringify(domain));
+        
+        }),
+        function(error) {
+          console.log(error);     
+        }
       client.events.on('app.activated', onAppActivate);
     }
   }
 };
 
+
+
 function onAppActivate() {
   var textElement = document.getElementById('apptext');
-  var getContact = client.data.get('contact');
+  //var getContact = client.data.get('contact');
   //getContact.then(showContact).catch(handleErr);
  
   var getCompany = client.data.get('company');
@@ -23,12 +38,64 @@ function onAppActivate() {
   function showContact(payload) {
     textElement.innerHTML = `Ticket created by ${payload.contact.name}`;
   }
+  
   function showCompany(payload) {
     textElement.innerHTML = `Company name : ${payload.company.name}`;
   }
 
-  document.getElementById("clickMe").onclick = function () { console.log('This is for Testing Purpose!'); };
+  document.getElementById("clickMe").onclick = function () { 
+    console.log('This is for Testing Purpose!');
+    var getContact = client.data.get('contact');
+    getContact.then(showContact).catch(handleErr);
+    getContact.then(getTickets).catch(handleErr);
+  };
 }
+
+var getTickets = function(){
+	var url = domain+'/api/v2/tickets';
+
+				$.ajax(
+				{
+					url: url,
+					type: 'GET',
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					headers: {
+						"Authorization": auth				
+					},
+					success: function(data) {											
+						console.log("All tickets:-" +JSON.stringify(data));
+            var obj = data;
+            //console.log(obj.length);
+						var i;var ids =[];
+						var counter=0;
+						//for (i in obj)
+						for (i=obj.length-1;i>=0;i--)
+						{
+						
+							counter++;
+							ids.push(obj[i].id);
+							//if(counter == 5)
+								//break;
+						}
+           // var select = document.getElementById("TicketNumber");
+           // alert('ids:-'+ids)
+           
+
+           document.getElementById("demo").innerHTML = `Al Ticket Ids are: ${ids}`; 
+for (var i=0;i<ids.length;i++){
+  $('<option/>').val(ids[i]).html(ids[i]).appendTo('#TicketNumber');
+}
+ 
+						
+					//	callback(Conversations,resp_id);
+					},
+					error: function() {
+						console.log("error");  
+                         callback("",resp_id);						
+					}
+				});
+};
 
 function handleErr(err) {
   console.error(`Error occured. Details:`, err);
